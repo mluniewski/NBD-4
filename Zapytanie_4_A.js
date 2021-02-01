33e9993206dcf525398f93cda09a4a4850304819
mluniewski <61694771+mluniewski@users.noreply.github.com>
2021-02-02 00:31:03 +0100

    NBD-4

---
+++ Zapytanie_4_A.js
@@ -0,0 +1,13 @@
+printjson(db.people.aggregate([
+	{
+		$addFields: {
+            weight: {$convert: { input: "$weight", to: "decimal", onError: Error }},
+            height: {$convert: { input: "$height", to: "decimal", onError: Error }} 
+        }
+        },
+{$group: {
+	_id: "$nationality", 
+	"maxBMI": {"$max" : { $divide:["$weight",{ $pow: [ { $divide: [ "$height", 100 ] }, 2 ] }]}},
+    "minBMI": {"$min" : { $divide:["$weight",{ $pow: [ { $divide: [ "$height", 100 ] }, 2 ] }]}},
+}}
+]).toArray())
